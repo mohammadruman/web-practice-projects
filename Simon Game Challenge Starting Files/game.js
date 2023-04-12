58ec5f33e6e80a1e8d9cc0aa53cf6d2dad3d65b0
Mohammad Ruman <mohammaderuman@gmail.com>
2023-04-13 03:24:06 +0530

    commited simon game

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,32 @@
-alert("hello");
+//3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
+
+var buttonColours = ["red", "blue", "green", "yellow"]
+//5. At the top of the game.js file, create a new empty array called gamePattern.
+
+var gamePattern =[];
+//1. Inside game.js create a new function called nextSequence()
+var userClickedPattern = [];
+//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+$(".btn").click(function() {
+
+  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+  var userChosenColour = $(this).attr("id");
+
+  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+  userClickedPattern.push(userChosenColour);
+
+  //console.log(userClickedPattern);
+
+});
+function nextSequence(){
+
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+      //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+}
+
+
