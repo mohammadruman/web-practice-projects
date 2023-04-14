6d713ba4d923aaa73af5c335460982e3cb8855bb
Mohammad Ruman <mohammaderuman@gmail.com>
2023-04-15 00:33:31 +0530

    node js

---
+++ nodeJs/index.js
@@ -0,0 +1,10 @@
+//jshint esversion:6
+// const superheroes = require('superheroes');
+// superheroes.all;
+
+// superheroes.random();
+// console.log(superheroes);
+
+var superheroes = require("superheroes");
+var mysupeheroes = superheroes.random();
+console.log(mysupeheroes);
